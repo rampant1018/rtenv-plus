9ec14990a0491236588a0b215a98328ca44ccca4
Rampant <rampant1018@gmail.com>
2014-03-17 10:36:29 +0800

    Define a new macro for build target.

--- mk/qemu.mk
+++ mk/qemu.mk
@@ -1,57 +1,58 @@
 QEMU_STM32 ?= ../qemu_stm32/arm-softmmu/qemu-system-arm
+BUILD_TARGET = $(OUTDIR)/$(TARGET)
 
-qemu: $(OUTDIR)/$(TARGET).bin $(QEMU_STM32)
+qemu: $(BUILD_TARGET).bin $(QEMU_STM32)
 	$(QEMU_STM32) -M stm32-p103 \
 		-monitor stdio \
-		-kernel $(OUTDIR)/$(TARGET).bin
+		-kernel $(BUILD_TARGET).bin
 
-qemugdb: $(OUTDIR)/$(TARGET).bin $(QEMU_STM32)
-	$(QEMU_STM32) -M stm32-p103 \
-		-monitor stdio \
-		-gdb tcp::3333 -S \
-		-kernel $(OUTDIR)/$(TARGET).bin
-
-qemudbg: $(OUTDIR)/$(TARGET).bin $(QEMU_STM32)
+qemugdb: $(BUILD_TARGET).bin $(QEMU_STM32)
 	$(QEMU_STM32) -M stm32-p103 \
 		-monitor stdio \
 		-gdb tcp::3333 -S \
-		-kernel $(OUTDIR)/$(TARGET).bin 2>&1>/dev/null & \
+		-kernel $(BUILD_TARGET).bin
+
+qemudbg: $(BUILD_TARGET).bin $(QEMU_STM32)
+	$(QEMU_STM32) -M stm32-p103 \
+		-monitor stdio \
+		-gdb tcp::3333 -S \
+		-kernel $(BUILD_TARGET).bin 2>&1>/dev/null & \
 	echo $$! > $(OUTDIR)/qemu_pid && \
 	$(CROSS_COMPILE)gdb -x $(TOOLDIR)/gdbscript && \
 	cat $(OUTDIR)/qemu_pid | `xargs kill 2>/dev/null || test true` && \
 	rm -f $(OUTDIR)/qemu_pid
 
-qemu_remote: $(OUTDIR)/$(TARGET).bin $(QEMU_STM32)
-	$(QEMU_STM32) -M stm32-p103 -kernel $(OUTDIR)/$(TARGET).bin -vnc :1
+qemu_remote: $(BUILD_TARGET).bin $(QEMU_STM32)
+	$(QEMU_STM32) -M stm32-p103 -kernel $(BUILD_TARGET).bin -vnc :1
 
-qemudbg_remote: $(OUTDIR)/$(TARGET).bin $(QEMU_STM32)
+qemudbg_remote: $(BUILD_TARGET).bin $(QEMU_STM32)
 	$(QEMU_STM32) -M stm32-p103 \
 		-gdb tcp::3333 -S \
-		-kernel $(OUTDIR)/$(TARGET).bin \
+		-kernel $(BUILD_TARGET).bin \
 		-vnc :1
 
-qemu_remote_bg: $(OUTDIR)/$(TARGET).bin $(QEMU_STM32)
+qemu_remote_bg: $(BUILD_TARGET).bin $(QEMU_STM32)
 	$(QEMU_STM32) -M stm32-p103 \
-		-kernel $(OUTDIR)/$(TARGET).bin \
+		-kernel $(BUILD_TARGET).bin \
 		-vnc :1 &
 
-qemudbg_remote_bg: $(OUTDIR)/$(TARGET).bin $(QEMU_STM32)
+qemudbg_remote_bg: $(BUILD_TARGET).bin $(QEMU_STM32)
 	$(QEMU_STM32) -M stm32-p103 \
 		-gdb tcp::3333 -S \
-		-kernel $(OUTDIR)/$(TARGET).bin \
+		-kernel $(BUILD_TARGET).bin \
 		-vnc :1 &
 
-emu: $(OUTDIR)/$(TARGET).bin
-	bash $(TOOLDIR)/emulate.sh $(OUTDIR)/$(TARGET).bin
+emu: $(BUILD_TARGET).bin
+	bash $(TOOLDIR)/emulate.sh $(BUILD_TARGET).bin
 
-qemuauto: $(OUTDIR)/$(TARGET).bin $(TOOLDIR)gdbscript
-	bash $(TOOLDIR)emulate.sh $(OUTDIR)/$(TARGET).bin &
+qemuauto: $(BUILD_TARGET).bin $(TOOLDIR)gdbscript
+	bash $(TOOLDIR)emulate.sh $(BUILD_TARGET).bin &
 	sleep 1
 	$(CROSS_COMPILE)gdb -x gdbscript&
 	sleep 5
 
-qemuauto_remote: $(OUTDIR)/$(TARGET).bin $(TOOLDIR)gdbscript
-	bash $(TOOLDIR)emulate_remote.sh $(OUTDIR)/$(TARGET).bin &
+qemuauto_remote: $(BUILD_TARGET).bin $(TOOLDIR)gdbscript
+	bash $(TOOLDIR)emulate_remote.sh $(BUILD_TARGET).bin &
 	sleep 1
 	$(CROSS_COMPILE)gdb -x gdbscript&
 	sleep 5
