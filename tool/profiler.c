221edc05f86d91df0ca002da41887c93a11b5f58
Rampant <rampant1018@gmail.com>
2014-03-25 10:48:23 +0800

    profiler: Use vcd file format instead of raw data.
    Now can use GTKWave to show the visualize execution status.

--- tool/profiler.c
+++ tool/profiler.c
@@ -11,9 +11,13 @@
 
 #define ERR(err) (ERR_##err)
 
+#define MKTL(n, d) {.task_name=#n, .task_identifier=d}
+#define TASK_COUNT (sizeof(task_list)/sizeof(task_list[0]))
+
 int main (int argc, char *argv[])
 {
     char c;
+    int i;
     FILE *outfile;
     FILE *infile;
     const char *outname = NULL;
@@ -25,6 +29,22 @@ int main (int argc, char *argv[])
     int tmpTime;
     int tmpCs;
     int tmpSysTick;
+
+    // task structure
+    struct task_t {
+        char *task_name;
+        char task_identifier;
+    };
+    const struct task_t task_list[] = {
+        MKTL("first", '!'),
+        MKTL("pathserver", '@'),
+        MKTL("romdev_driver", '#'),
+        MKTL("romfs_server", '$'),
+        MKTL("serialout", '%'),
+        MKTL("serialin", '^'),
+        MKTL("rs232_xmit_msg_task", '&'),
+        MKTL("shell_task", '*'),
+    };
 
     while ((c = getopt(argc, argv, "o:d:")) != -1) {
         switch (c) {
@@ -47,7 +67,6 @@ int main (int argc, char *argv[])
         }
     }
 
-    //printf("%d %d %d %d %d\n", sizeof(tmpTCB.stack), sizeof(tmpTCB.pid), sizeof(tmpTCB.status), sizeof(tmpTCB.priority), sizeof(tmpTCB.list));
     char tmpTrash[32];
 
     infile = fopen(inname, "r");
@@ -57,6 +76,23 @@ int main (int argc, char *argv[])
         return -1;
     }
     else {
+        // Header section
+        fprintf(outfile, "$version\n$end\n");
+        fprintf(outfile, "$comment\n$end\n");
+        fprintf(outfile, "$timescale 1us $end\n");
+
+        // Varaible definition section
+        for(i = 0; i < TASK_COUNT; i++) {
+            fprintf(outfile, "$var wire 1 %c %s $end\n", task_list[i].task_identifier, task_list[i].task_name);
+        }
+
+        // $dumpvars section
+        fprintf(outfile, "$dumpvars\n");
+        for(i = 0; i < TASK_COUNT; i++) {
+            fprintf(outfile, "0%c\n", task_list[i].task_identifier);
+        }
+        fprintf(outfile, "$end\n");
+
         while(!feof(infile)) {
             fread(&tmpCs, sizeof(int), 1, infile);
             fread(tmpTrash, 4, 1, infile);
@@ -67,7 +103,22 @@ int main (int argc, char *argv[])
             fread(&tmpStack, sizeof(struct user_thread_stack), 1, infile);
             fread(&tmpTime, sizeof(int), 1, infile);
             fread(&tmpSysTick, sizeof(int), 1, infile);
-            fprintf(outfile, "%5d %5d.%d:pid: %d, status: %d, priority: %d, ABI: %d\n", tmpCs, tmpTime, tmpSysTick, tmpTCB.pid, tmpTCB.status, tmpTCB.priority, tmpStack.r7);
+            float tmpTimeSec = tmpSysTick / 720000.0 + tmpTime;
+
+            if(tmpTime == 0 && tmpSysTick == 719999) { // Skip the incorrent section
+                continue;
+            }
+
+            //fprintf(outfile, "%5d %f:pid: %d, status: %d, priority: %d, ABI: %d\n", tmpCs, tmpTimeSec, tmpTCB.pid, tmpTCB.status, tmpTCB.priority, tmpStack.r7);
+            fprintf(outfile, "#%d\n", (int)(tmpTimeSec * 1000000.0));
+            for(i = 0; i < TASK_COUNT; i++) {
+                if(tmpTCB.pid == i) {
+                    fprintf(outfile, "1%c\n", task_list[i].task_identifier);
+                }
+                else {
+                    fprintf(outfile, "0%c\n", task_list[i].task_identifier);
+                }
+            }
         }
     }
 
